de3c9c03d7339eb229d5652eae2da9992783f9c3
Chris <cmuschek@gmail.com>
2014-11-15 13:48:02 -0600

    Debugged the Heuristic class

--- heuristic.py
+++ heuristic.py
@@ -5,20 +5,28 @@ class Heuristic:
 	def __init__ (self, length):
 		self.tableLen = length
 
-		# calculate the constant values ahead of time
-		for i in range(0, self.length):
-			self.constants[i] = i+1
 
-	def getValue (board):
+		# calculate the constant values ahead of time
+		self.constants = []
+		for i in range(0, length):
+			self.constants.append(i+1)
+
+	def getValue (self, board):
 		p1Bins = board.p1
-		p2Bins = reversed(board.p2)
+		p2Bins = board.p2
 
 		p1Sum = 0
 		p2Sum = 0
 
-		for i in range(0, self.length):
-
+		for i in range(0, self.tableLen):
 			p1Sum = p1Sum + (p1Bins[i] * self.constants[i])
-			p2Sum = p2Sum + (p2Bins[i] * self.constants[i])
+			p2Sum = p2Sum + (p2Bins[self.tableLen - (i + 1)] 
+				* self.constants[self.tableLen - (i + 1)])
 
 		return p1Sum - p2Sum
+
+
+if __name__ == "__main__":
+	board = Board(10, 4)
+	h = Heuristic(10)
+	print h.getValue(board)
