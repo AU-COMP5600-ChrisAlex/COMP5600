51011c99b8ca128fb94b9d168a336978eb4e1641
Alex Dyke <dykeag@gmail.com>
2014-11-19 12:43:41 -0600

    Added command line args

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ import curses
 import alpha_beta
 import human_player
 import and_or_player
+import sys, getopt
 
 
+askUser = False
 
 def startProg(screen):
     
+    global askUser
 
-    ui = UI(screen);
+    ui = UI(screen,askUser);
     b = Board();
     ui.drawState(b)
 
@@ -35,6 +38,29 @@ def startProg(screen):
 
 
 if __name__ == "__main__":
+    global askUser
+
+    argv = sys.argv[1:]
+
+    try:
+        opts, args = getopt.getopt(argv,"hd",["help","defaults"])
+    except getopt.GetoptError:
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print "-h [--help]    : Print this message"  
+            print "-d [defaults]  : For debug. Sets user input values to defaults"
+            sys.exit()
+        elif opt in ("-d", "--defaults"):
+            gameconstants.numRows = 4 
+            gameconstants.numPebbles = 4
+            gameconstants.p1Human = False
+            gameconstants.p2Human = False
+            gameconstants.numPlys = 4
+            gameconstants.stepThrough = False
+            askUser = False
+
     try:
         curses.wrapper(startProg)
     except KeyboardInterrupt:
