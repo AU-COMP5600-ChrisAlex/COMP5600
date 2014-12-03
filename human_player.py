7236e372d50843b9ba0a03325260743c074bf9d5
Alex Dyke <dykeag@gmail.com>
2014-12-02 18:05:58 -0600

    Fixed type in human_player which broke ui.interact

--- human_player.py
+++ human_player.py
@@ -9,7 +9,7 @@ class Human_Player(Player):
         self.ui = ui
 
     def move(self, board):
-        return self.ui.interact(self.player)
+        return self.ui.interact(self.player, board)
 
     def __str__(self):
         return "Human Player"
